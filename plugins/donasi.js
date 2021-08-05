6b493efb2871d95ce128c4a6cdff9a3d2b302e09
ztfcode <75156853+ztfcode@users.noreply.github.com>
2021-08-05 23:21:09 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Indosat Ooredoo [null]
+│ • Telkomsel [null]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
+│ • Gopay, OVO, Dana [null]
+│ • https://
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
